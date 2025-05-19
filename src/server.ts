7cbe271df010c2a8945d58097bf92724314760dd
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-05-19 14:00:13 -0300

    config express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Lucas Dev")
+import express, {json, Request, Response} from "express";
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(json())
+
+app.get("/", (req:Request, res:Response)=>{
+    res.status(200).json({player: "beckham"});
+});
+
+app.listen(port, ()=>{
+    console.log(`🔥 Server Runing at port http://localhost:${port}`);
+});
