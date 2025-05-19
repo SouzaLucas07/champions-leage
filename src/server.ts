ce843e140bf7c1eb5ad5ea4244028af0862b6ff8
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-05-19 16:10:23 -0300

    atualização da arquitetura

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import express, {json, Request, Response} from "express";
+import express from "express";
+import createApp from "./app";
 
+const app = createApp();
 const port = process.env.PORT;
-const app = express();
-
-app.use(json())
-
-app.get("/", (req:Request, res:Response)=>{
-    res.status(200).json({player: "beckham"});
-});
 
 app.listen(port, ()=>{
     console.log(`🔥 Server Runing at port http://localhost:${port}`);
